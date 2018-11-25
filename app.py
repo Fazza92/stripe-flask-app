98e3fa8c11af2550e39a2e0006dbaaa100b8e0ff
Oscar <oscarwasil@gmail.com>
2018-11-25 13:17:16 -0600

    Updated index template

--- app.py
+++ app.py
@@ -6,3 +6,31 @@ app = Flask(__name__)
 public_key = "pk_test_TYooMQauvdEDq54NiTphI7jx SECRET_KEY=sk_test_4eC39HqLyjWDarjtT1zdp7dc"
 
 stripe.api_key = "sk_test_BQokikJOvBiI2HlWgH4olfQ2"
+
+@app.route('/')
+def index():
+    return render_template('index.html', public_key=public_key)
+
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html')
+
+@app.route('/payment', methods=['POST'])
+def payment():
+
+    
+    customer = stripe.Customer.create(email=request.form['stripeEmail'],
+                                      source=request.form['stripeToken'])
+
+    
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=1999,
+        currency='usd',
+        description='Donation'
+    )
+
+    return redirect(url_for('thankyou'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
